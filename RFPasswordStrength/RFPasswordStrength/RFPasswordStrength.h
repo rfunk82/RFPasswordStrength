938233fa28aa1383dedafb7490ce60f4e83e9b81
rfunk82 <rfunk82@gmail.com>
2013-12-09 11:57:01 -0500

    Initial setup

---
+++ RFPasswordStrength/RFPasswordStrength/RFPasswordStrength.h
@@ -0,0 +1,13 @@
+//
+//  RFPasswordStrength.h
+//  RFPasswordStrength
+//
+//  Created by Ricardo Funk on 12/9/13.
+//  Copyright (c) 2013 Ricardo Funk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RFPasswordStrength : NSObject
+
+@end
---
+++ RFPasswordStrength/RFPasswordStrength/RFPasswordStrength.m
@@ -0,0 +1,13 @@
+//
+//  RFPasswordStrength.m
+//  RFPasswordStrength
+//
+//  Created by Ricardo Funk on 12/9/13.
+//  Copyright (c) 2013 Ricardo Funk. All rights reserved.
+//
+
+#import "RFPasswordStrength.h"
+
+@implementation RFPasswordStrength
+
+@end
